53e456da2cc862e39b43918e2afad55df4000d67
Anisimova Karina <51861302+Kira5005-code@users.noreply.github.com>
2021-10-27 15:31:22 +0300

    Add files via upload

---
+++ dataset/base/file1_format.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    a=5;
+    b=4;
+    if (a>4){
+    	cout<<a-b;
+    }else{
+        cout<<a+b;
+    }
+    return 0;
+}
---
+++ dataset/base/file1_rename.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int k, l;
+    k = 5;
+    l = 4;
+    if (k > 4) {
+    	cout << k - l;
+    } else {
+        cout << k + l;
+    }
+    return 0;
+}
